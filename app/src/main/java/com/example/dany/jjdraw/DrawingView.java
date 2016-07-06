37cccfe4e4ea7b83ccb98699372e03240fb30a90
dany madden <dany4madden@gmail.com>
2016-07-06 11:45:21 -0700

    Initial commit

---
+++ app/src/main/java/com/example/dany/jjdraw/DrawingView.java
@@ -0,0 +1,18 @@
+package com.example.dany.jjdraw;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by dany on 6/30/16.
+ */
+public class DrawingView extends View {
+    public DrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setupDrawing();
+    }
+    private void setupDrawing() {
+
+    }
+}
