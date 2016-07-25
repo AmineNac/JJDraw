2185b7e7468fef4bfe4f5fa033996fb5a058744c
dany madden <dany4madden@gmail.com>
2016-07-25 15:35:25 -0700

    Added app credit to the about page in the overflow menu

--- app/src/main/java/com/example/dany/jjdraw/About.java
+++ app/src/main/java/com/example/dany/jjdraw/About.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.text.method.LinkMovementMethod;
 
 /**
  * Copyright (c) 2016 Dany Madden
@@ -16,6 +18,9 @@ public class About extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        TextView t2 = (TextView) findViewById(R.id.credit);
+        t2.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
 }
